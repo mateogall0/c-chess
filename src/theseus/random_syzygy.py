19644bdf9fd7887171ee09d078374a9ed1cbbde2
gallit0 <mateogesede@gmail.com>
2023-11-29 00:23:00 -0300

    WIP

--- src/theseus/random_syzygy.py
+++ src/theseus/random_syzygy.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import chess
 import chess.svg
-import random
+import numpy as np
 
 
 def count_pieces(board):
@@ -15,18 +15,26 @@ def count_pieces(board):
     return total_pieces
 
 
-def random_fen(limit=random.randint(3, 7), board=chess.Board()):
+def random_fen(limit, board=chess.Board()):
     while 1:
         legal_moves = [move for move in board.legal_moves]
         if not legal_moves:
             break
 
-        random_move = random.choice(legal_moves)
+        random_move = np.random.choice(legal_moves)
         board.push(random_move)
-        if count_pieces(board) <= limit:
+        pieces = count_pieces(board)
+        if pieces <= limit:
             break
-    return board.fen(), board.is_game_over()
+    return board.fen(), board.is_game_over(), pieces
 
 
-random_fen = random_fen()
-print("Random FEN:", random_fen)
+if __name__ == '__main__':
+    limit = 3
+    for _ in range(5):
+        fen, is_over, pieces = random_fen(limit=limit)
+        limit += 1
+        if limit > 7: limit = 3
+        print(limit)
+        print("Random FEN:", fen, is_over, pieces)
+        print(chess.Board(fen))
