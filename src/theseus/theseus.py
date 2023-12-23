7158878b78e932d13b06b4c121a74d45793d2fee
gallit0 <mateogesede@gmail.com>
2023-12-23 17:51:36 -0300

    Add: functions to create models

--- src/theseus/theseus.py
+++ src/theseus/theseus.py
@@ -3,6 +3,7 @@
 from tensorflow import keras as K
 import numpy as np
 import chess
+import model
 
 class Theseus:
     max_moves = 128
@@ -42,36 +43,16 @@ class Theseus:
         return self.__engine.save(path)
 
     def new_model(self):
-        color_input_layer = K.layers.Input(shape=(1,), dtype='int32')
-        board_input_layer = K.layers.Input(shape=(8, 8))
-        moves_input_layer = K.layers.Input(shape=(1, self.max_moves))
+        (merged_inputs, color_input_layer, board_input_layer,
+         moves_input_layer) = model.input_layers(self.max_moves)
 
-        board_flattened = K.layers.Flatten()(board_input_layer)
-        moves_flattened = K.layers.Flatten()(moves_input_layer)
+        output_layer = model.hidden_layers(self.max_moves, merged_inputs)
 
-        hidden_layer_board = K.layers.Dense(64, activation='relu')(board_flattened)
-        hidden_layer_moves = K.layers.Dense(self.max_moves, activation='relu')(moves_flattened)
-        hidden_layer_color = K.layers.Dense(32, activation='relu')(K.layers.Flatten()(K.layers.Embedding(2, 32)(color_input_layer)))
-
-        merged_inputs = K.layers.concatenate([hidden_layer_color, hidden_layer_board, hidden_layer_moves])
-
-        hidden_layer = K.layers.Dense(64 + self.max_moves + 32, activation='relu')(merged_inputs)
-        hidden_layer = K.layers.BatchNormalization()(hidden_layer)
-        hidden_layer = K.layers.Dense(64 + self.max_moves + 32, activation='relu')(hidden_layer)
-        hidden_layer = K.layers.BatchNormalization()(hidden_layer)
-        hidden_layer = K.layers.Dense(64 + self.max_moves + 32, activation='relu')(hidden_layer)
-        hidden_layer = K.layers.BatchNormalization()(hidden_layer)
-        hidden_layer = K.layers.Dense(64 + self.max_moves + 32, activation='relu')(hidden_layer)
-        hidden_layer = K.layers.BatchNormalization()(hidden_layer)
-        hidden_layer = K.layers.Dense(64 + self.max_moves + 32, activation='relu')(hidden_layer)
-        hidden_layer = K.layers.BatchNormalization()(hidden_layer)
-        hidden_layer = K.layers.Dense(64 + self.max_moves + 32, activation='relu')(hidden_layer)
-        hidden_layer = K.layers.BatchNormalization()(hidden_layer)
-        output_layer = K.layers.Dense(self.max_moves, activation='softmax')(hidden_layer)
-
-        model = K.models.Model(inputs=[color_input_layer, board_input_layer, moves_input_layer], outputs=output_layer)
-        model.compile(loss='categorical_crossentropy', optimizer=K.optimizers.Adam(), metrics=['accuracy'])
-        return model
+        m = K.models.Model(
+            inputs=[color_input_layer, board_input_layer, moves_input_layer], outputs=output_layer
+        )
+        m.compile(loss='categorical_crossentropy', optimizer=K.optimizers.Adam(), metrics=['accuracy'])
+        return m
 
     def default_session_train(self):
         fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
