e1a61a61ea809e3aa0b544d721ccd31dc7fd4630
gallit0 <mateogesede@gmail.com>
2023-12-30 09:42:30 -0300

    Add: random syzygy generator

--- src/theseus/random_syzygy.py
+++ src/theseus/random_syzygy.py
@@ -49,22 +49,22 @@ def random_fen():
             break
     return board.fen(), board.is_game_over(), pieces
 
-def random_syzygy(verbose=True, iterations=10):
-    fen_codes = []
+def random_syzygy(verbose=True, iterations=14):
     fen_codes_readable = []
     for _ in range(iterations):
         fen, is_over, _ = random_fen()
         if not is_over:
-            fen_codes.append(fen.replace(' ', '_'))
             fen_codes_readable.append(fen)
-    fen_codes = remove_redundancies(fen_codes)
+    fen_codes_readable = remove_redundancies(fen_codes_readable)
+    fen_codes = [f.replace(' ', '_') for f in fen_codes_readable]
     if verbose:
         print(fen_codes)
         print(len(fen_codes))
     return fen_codes, fen_codes_readable
 
 def get_syzygy_output(fen_codes=[], fen_codes_readable=[],
-                      url='http://tablebase.lichess.ovh/standard?fen='):
+                      url='http://tablebase.lichess.ovh/standard?fen=',
+                      verbose=True):
     Y = []
     X0 = []
     X1 = []
@@ -74,19 +74,14 @@ def get_syzygy_output(fen_codes=[], fen_codes_readable=[],
         cat = res['category']
         if cat != 'win' and cat != 'cursed-win' and cat != 'maybe-win' and cat != 'draw':
             continue
-        uci = list(res['moves'][0]['uci'])
-        uci[0] = files[uci[0]]
-        uci[2] = files[uci[2]]
-        modified_string = ''.join(uci)
-        Y.append(int(''.join(modified_string[:4])))
         board = chess.Board(fen_codes_readable[i])
         who_moves = 1 if board.turn == chess.WHITE else 0
         X0.append(who_moves)
         X1.append(Theseus.board_to_bitboard(fen=fen_codes_readable[i]))
         possible_moves = [0 for _ in range(Theseus.max_moves)]
-        for c, i in enumerate(board.legal_moves):
+        for c, moves in enumerate(board.legal_moves):
             current_move = 0
-            for j, item in enumerate(str(i)):
+            for j, item in enumerate(str(moves)):
                 if j % 2 == 0:
                     try:
                         current_move += 10 ** j * Theseus.files[item]
@@ -96,6 +91,23 @@ def get_syzygy_output(fen_codes=[], fen_codes_readable=[],
                 current_move += 10 ** j * int(item)
             possible_moves[c] = current_move
         X2.append(possible_moves)
+        uci_s = res['moves'][0]['uci'][:4]
+        chosen_move = np.zeros((Theseus.max_moves,))
+        for j, item in enumerate(board.legal_moves):
+            if uci_s == str(item)[:4]:
+                chosen_move[j] = 1
+                break
+        else:
+            print(f'Fen: {fen_codes[i]}')
+            print(f'Fen readable: {fen_codes_readable[i]}')
+            raise ValueError(f'{uci_s} not found in legal moves: {list(board.legal_moves)}')
+        Y.append(chosen_move)
+
+    if verbose:
+        print(f'X0 len: {len(X0)}')
+        print(f'X1 len: {len(X1)}')
+        print(f'X2 len: {len(X2)}')
+        print(f'Y  len: {len(Y)}')
     return (
         np.array(X0),
         np.array(X1),
@@ -106,5 +118,5 @@ def get_syzygy_output(fen_codes=[], fen_codes_readable=[],
 
 if __name__ == '__main__':
     fen_codes, fen_codes_readable = random_syzygy()
-    Y = get_syzygy_output(fen_codes, fen_codes_readable)
-    print(Y)
+    X0, X1, X2, Y = get_syzygy_output(fen_codes, fen_codes_readable)
+    np.savez('val_data/syzygy.npz', X0=X0, X1=X1, X2=X2, Y=Y)
