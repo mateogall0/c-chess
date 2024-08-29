1e8c3620112ee99e1a7ab351e707fe33e251d45c
mateogall0 <mateogesede@gmail.com>
2024-08-29 08:41:58 -0300

    wip

---
+++ tests/test_chess_wrapper.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from unittest import TestCase
+from src.theseus_v2.wrappers import ChessWrapper
+import chess
+import numpy as np
+
+
+class TestChessWrapper(TestCase):
+    def test_board_to_array(self):
+        fen = 'rnbqk2r/ppp2ppp/4pn2/3p4/1bPP4/2N5/PP1BPPPP/R2QKBNR w KQkq - 4 5'
+        board = chess.Board(fen=fen)
+        arr = ChessWrapper.board_to_array(board)
+        self.assertIsInstance(arr, np.ndarray)
+        ret_board = ChessWrapper.array_to_board(arr)
+        self.assertIsInstance(ret_board, chess.Board)
+        self.assertEqual(ret_board.fen(), fen)
