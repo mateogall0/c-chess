bf63081093d630378f43c364aec28163d5bf7b32
mateogall0 <mateogesede@gmail.com>
2024-09-05 13:16:06 -0300

    update tests

--- tests/test_evaluator.py
+++ tests/test_evaluator.py
@@ -8,9 +8,13 @@ class TestEvaluator(TestCase):
     def test_capture(self):
         ev = Evaluator()
         bf = chess.Board('rnbqkbnr/ppp1pppp/8/3p4/4P3/8/PPPP1PPP/RNBQKBNR w KQkq d6 0 2')
-        ba = chess.Board('rnbqkbnr/ppp1pppp/8/3P4/8/8/PPPP1PPP/RNBQKBNR b KQkq - 0 2')
-        r = ev.evaluate_capture(bf, ba)
+        m = chess.Move.from_uci('e4d5')
+        r = ev.evaluate_capture(bf, m)
         self.assertEqual(r, 0.1)
+        bf = chess.Board('rnbqkbnr/ppp1p1pp/8/3p1p2/4P1Q1/7P/PPPP1PP1/RNB1KBNR b KQkq - 0 3')
+        m = chess.Move.from_uci('f5g4')
+        r = ev.evaluate_capture(bf, m)
+        self.assertEqual(r, 0.9)
 
     def test_no_capture(self):
         ev = Evaluator()
