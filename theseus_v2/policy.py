7743abc97c0a55bb9183939474cdf98a85021f46
mateogall0 <mateogesede@gmail.com>
2024-09-17 16:31:58 -0300

    wip

--- theseus_v2/policy.py
+++ theseus_v2/policy.py
@@ -3,16 +3,26 @@ import torch
 from stable_baselines3.ppo import MlpPolicy
 from theseus_v2.wrappers import ChessWrapper2
 
-
 class CustomPolicy(MlpPolicy):
-    def forward(self, obs, deterministic=False):
-        actions = super().forward(obs, deterministic)
-        mask = self.get_action_mask(obs)
-        masked_actions = actions * mask
-        return masked_actions
-    
+    def forward(self, obs: torch.Tensor, deterministic: bool = False) -> tuple:
+        latent_pi, latent_vf, latent_sde = self._get_latent(obs)
+        values = self.value_net(latent_vf)
+        distribution = self._get_action_dist_from_latent(latent_pi, latent_sde=latent_sde)
+        action_mask = self.get_action_mask(obs)
+        action_probs = distribution.distribution.logits
+        print(dir(action_probs))
+        masked_probs = action_probs * action_mask.unsqueeze(1)
+        masked_probs = masked_probs / masked_probs.sum(dim=1, keepdim=True)
+        actions = masked_probs.argmax(dim=1) if deterministic else masked_probs.multinomial(num_samples=1).squeeze()
+        log_prob = torch.log(masked_probs.gather(1, actions.unsqueeze(1)))
+        
+        return actions, values, log_prob
+
     def get_action_mask(self, obs):
-        for i in obs:
-            b = ChessWrapper2.array_to_board(i)
-            
-        print(b)
+        batch_size = len(obs)
+        masks = torch.zeros((batch_size, self.action_space.n), dtype=torch.float32)
+        for i in range(batch_size):
+            _, valid_moves = ChessWrapper2.array_to_board(obs[i])
+            num_valid_moves = len(valid_moves)
+            masks[i, :num_valid_moves] = 1
+        return masks
